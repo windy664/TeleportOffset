e6b00d6e0f2ce5d136776f7d095a5cd40d015560
windy <81475218+windy664@users.noreply.github.com>
2023-08-31 16:35:42 +0800

    Add files via upload

---
+++ src/main/java/org/windy/teleportoffset/TeleportOffset.java
@@ -0,0 +1,75 @@
+package org.windy.teleportoffset;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
+
+public class TeleportOffset extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        this.getServer().getPluginManager().registerEvents(this, this);
+        String version = this.getDescription().getVersion();
+        String serverName = this.getServer().getName();
+        this.getServer().getConsoleSender().sendMessage("§6+--------------------------------------+\n");
+        this.getServer().getConsoleSender().sendMessage(" ______        __                         __   ____    ___   ___            __ ");
+        this.getServer().getConsoleSender().sendMessage("/_  __/ ___   / / ___    ___  ___   ____ / /_ / __ \\  / _/  / _/  ___ ___  / /_");
+        this.getServer().getConsoleSender().sendMessage(" / /   / -_) / / / -_)  / _ \\/ _ \\ / __// __// /_/ / / _/  / _/  (_-</ -_)/ __/");
+        this.getServer().getConsoleSender().sendMessage("/_/    \\__/ /_/  \\__/  / .__/\\___//_/   \\__/ \\____/ /_/   /_/   /___/\\__/ \\__/ ");
+        this.getServer().getConsoleSender().sendMessage("                      /_/     \n");
+        this.getServer().getConsoleSender().sendMessage("§a"+ version + " §e " + serverName + "\n");
+        this.getServer().getConsoleSender().sendMessage("§6+--------------------------------------+");
+        // Plugin startup logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("teleportoffset")) {
+            if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission("teleportoffset.reload")) {
+                    sender.sendMessage("§c你没有权限来执行这个指令!");
+                    return true;
+                }
+
+                this.reloadConfig();
+                sender.sendMessage("§aTeleportOffset 配置已重新加载!");
+                return true;
+            } else {
+                sender.sendMessage("§c用法: /teleportoffset reload");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        double offsetX = this.getConfig().getDouble("teleport-offset.x");
+        double offsetY = this.getConfig().getDouble("teleport-offset.y");
+        double offsetZ = this.getConfig().getDouble("teleport-offset.z");
+        Location oldlocation = Objects.requireNonNull(event.getFrom()).clone();
+        Location location = Objects.requireNonNull(event.getTo()).clone();
+        location.add(offsetX, offsetY, offsetZ);
+        this.getLogger().info("玩家 " + event.getPlayer().getName() + " 从 " + oldlocation + "传送到: " + location.toString());
+        event.setTo(location);
+    }
+
+    @Override
+    public void onDisable() {
+        this.getServer().getConsoleSender().sendMessage("§6+--------------------------------------+\n");
+        this.getServer().getConsoleSender().sendMessage(" ______        __                         __   ____    ___   ___            __ ");
+        this.getServer().getConsoleSender().sendMessage("/_  __/ ___   / / ___    ___  ___   ____ / /_ / __ \\  / _/  / _/  ___ ___  / /_");
+        this.getServer().getConsoleSender().sendMessage(" / /   / -_) / / / -_)  / _ \\/ _ \\ / __// __// /_/ / / _/  / _/  (_-</ -_)/ __/");
+        this.getServer().getConsoleSender().sendMessage("/_/    \\__/ /_/  \\__/  / .__/\\___//_/   \\__/ \\____/ /_/   /_/   /___/\\__/ \\__/ ");
+        this.getServer().getConsoleSender().sendMessage("                      /_/     \n");
+        this.getServer().getConsoleSender().sendMessage("已卸载！\n");
+        this.getServer().getConsoleSender().sendMessage("§6+--------------------------------------+");
+    }
+}
