008422deaf42ecc57ffb73b459acb096a258df43
Windy 风吟 <81475218+windy664@users.noreply.github.com>
2024-08-09 22:09:58 +0800

    史家之绝唱

--- src/main/java/org/windy/teleportoffset/TeleportOffset.java
+++ src/main/java/org/windy/teleportoffset/TeleportOffset.java
@@ -1,20 +1,14 @@
 package org.windy.teleportoffset;
 
-import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.List;
 
 
 public class TeleportOffset extends JavaPlugin implements Listener {
@@ -22,6 +16,7 @@ public class TeleportOffset extends JavaPlugin implements Listener {
     String value = this.getConfig().getString("Debug");
     boolean debugMode = Boolean.parseBoolean(value);
     private static TeleportOffset instance;
+    private String Prefix;
     @Override
     public void onEnable() {
         instance = this;
@@ -37,6 +32,7 @@ public class TeleportOffset extends JavaPlugin implements Listener {
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
             this.getServer().getConsoleSender().sendMessage("检测到PlaceholderAPI，已兼容！");
         }
+        Prefix = this.getConfig().getString("Prefix");
     }
     public static TeleportOffset getInstance() {
         return instance;
@@ -103,5 +99,22 @@ public class TeleportOffset extends JavaPlugin implements Listener {
         this.getServer().getConsoleSender().sendMessage("已卸载！\n");
         this.getServer().getConsoleSender().sendMessage("§6+--------------------------------------+");
     }
-    
+
+
+    public boolean isdebugMode() {
+        return debugMode;
+    }
+
+    public void setdebugMode(boolean debugMode) {
+        this.debugMode = debugMode;
+    }
+
+    public void toggleQueryMode() {
+        setdebugMode(!isdebugMode());
+    }
+    public String prefix() {
+        return Prefix;
+    }
+
+
 }
