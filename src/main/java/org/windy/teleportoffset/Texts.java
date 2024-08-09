ee47d0acb4252ce7783cffc2e2ede3b909cbdf34
Windy 风吟 <81475218+windy664@users.noreply.github.com>
2024-08-09 15:45:58 +0800

    更新help帮助页

--- src/main/java/org/windy/teleportoffset/Texts.java
+++ src/main/java/org/windy/teleportoffset/Texts.java
@@ -13,4 +13,22 @@ public class Texts {
                                         | |                                                                \s
                                         |_|      \s
             """;
+
+    public static final String help = """
+            
+            
+            /teleportoffset reload 👀：
+            作用🎯：重新加载 TeleportOffset 的配置。
+            权限要求👮‍♂️：需要您具有 teleportoffset.reload 权限。
+            示例💡：当您拥有权限并输入此指令时，系统会重新加载配置，并告知您 §aTeleportOffset 配置已重新加载! 。
+            /teleportoffset debug 👀：
+            作用🎯：切换 debug 模式。
+            权限要求👮‍♂️：需要您具有 teleportoffset.debug 权限。
+            示例💡：当您拥有权限并输入此指令时，系统会告知您 §aDebug 模式已临时 加上当前的状态，比如 开启 或 关闭 。
+            /teleportoffset top 👀：
+            作用🎯：根据您当前的位置，找到最高的非空气方块位置，并将您传送到那里。
+            示例💡：输入此指令后，您会被传送到相应的位置。
+            
+            
+            """;
 }
